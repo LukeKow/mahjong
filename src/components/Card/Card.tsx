983ce33d1631883f1c48c9a19c7620071c7a48b2
Lukasz Kowalczyk <lkowalczyk@objectivity.co.uk>
2019-06-11 08:02:24 +0200

    Card class component changed to functional component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,43 +2,77 @@ import ICardProps from "./ICardProps";
 import * as React from 'react';
 import './style/Card.css';
 
-export default class Card extends React.Component<ICardProps>{
+const Card = (props: ICardProps) => {
 
-    constructor(props: ICardProps) {
-        super(props);
+    const handleClick = () => props.handleClick(props.id);
 
-        this.handleClick = this.handleClick.bind(this);
+    const renderCard = () => {
+        return props.headsOnTop ?
+            renderHeadsOnTop() :
+            renderTailsOnTop();
     }
 
-    public render() {
-        return (
-            <div className="card" onClick={this.handleClick} >
-                {this.renderCard()}
-            </div>
-        );
-    }
-
-    private handleClick = () => {
-        this.props.handleClick(this.props.id);
-    }
-
-    private renderCard() {
-        return this.props.headsOnTop ?
-            this.renderHeadsOnTop() :
-            this.renderTailsOnTop();
-    }
-    private renderTailsOnTop = (): JSX.Element => {
+    const renderTailsOnTop = (): JSX.Element => {
         return (
             <div className="cardValue">
-                {this.props.tailsValue}
+                { props.tailsValue }
             </div>
         );
     }
-    private renderHeadsOnTop = (): JSX.Element => {
+
+    const renderHeadsOnTop = (): JSX.Element => {
         return (
             <div className="cardValue">
-                {this.props.headsValue}
+                { props.headsValue }
             </div>
         );
     }
+
+    return(
+        <div className="card" onClick={handleClick} >
+                {renderCard()}
+        </div>
+    );
 }
+export default Card;
+
+// class Card extends React.Component<ICardProps>{
+
+//     constructor(props: ICardProps) {
+//         super(props);
+
+//         this.handleClick = this.handleClick.bind(this);
+//     }
+
+//     public render() {
+//         return (
+//             <div className="card" onClick={this.handleClick} >
+//                 {this.renderCard()}
+//             </div>
+//         );
+//     }
+
+//     private handleClick = () => {
+//         this.props.handleClick(this.props.id);
+//     }
+
+//     private renderCard() {
+//         return this.props.headsOnTop ?
+//             this.renderHeadsOnTop() :
+//             this.renderTailsOnTop();
+//     }
+//     private renderTailsOnTop = (): JSX.Element => {
+//         return (
+//             <div className="cardValue">
+//                 {this.props.tailsValue}
+//             </div>
+//         );
+//     }
+//     private renderHeadsOnTop = (): JSX.Element => {
+//         return (
+//             <div className="cardValue">
+//                 {this.props.headsValue}
+//             </div>
+//         );
+//     }
+// }
