908dbd71d049590cb5c9130ecdde7bf254810968
lukasz kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2019-06-24 22:06:09 +0200

    first test routes added

---
+++ src/components/main/Main.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Board from '../board/Board';
+import Home from '../home/Home';
+export default class Main extends React.Component<{},{}> {
+    render() {
+        return (
+            <Router>
+                <div className="container">
+                    <Route exact={true} path="/" component={Home} />
+                    <Route path="/board" component={Board} />                    
+                </div>
+            </Router>
+        );
+    }
+}
