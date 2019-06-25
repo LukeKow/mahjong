dc48ed181bd468c3d146ba5df0d8c2b2e0b0e18f
lukasz kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2019-06-25 22:26:18 +0200

    first route onclick

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 
-export default class Home extends React.Component<{},{}>{
+class Home extends React.Component<RouteComponentProps,{}>{
+    private clickHandler = () => {
+        this.props.history.push("/board");
+    }
+
     render(){
         return(
-            <div>HOME PAGE</div>
+            <div>
+                <div>HOME PAGE</div>
+                <div><button onClick={this.clickHandler}>Board</button></div>
+            </div>
+            
         );
     }
 }
+
+export default withRouter(Home);
